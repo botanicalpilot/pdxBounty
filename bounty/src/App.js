e15b5154692b27ce645499246b906d90f85da531
Akins <smo244@gmail.com>
2020-03-01 12:15:52 -0800

    began work on React app, components

--- bounty/src/App.js
+++ bounty/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+
+import Navbar from "./components/navbar.component";
+import PlantsList from "./components/plants-list.component";
+import EditPlant from "./components/edit-plant.component";
+import CreatePlant from "./components/create-plant.component";
+import CreateUser from "./components/create-user.component"; 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={PlantsList} />
+      <Route path="/edit/:id" exact component={EditPlant} />
+      <Route path="/create" exact component={CreatePlant} />
+      <Route path="/user" exact component={CreateUser} />
+    </Router>
   );
 }
 
